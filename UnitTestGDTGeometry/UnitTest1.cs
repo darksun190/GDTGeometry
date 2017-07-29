2f240c0210b9e29dafc86feb9f23307a8c40ddfe
sun xin <darksun190@hotmail.com>
2017-07-29 20:41:11 +0800

    单元测试Point类create test

--- UnitTestGDTGeometry/UnitTest1.cs
+++ UnitTestGDTGeometry/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GDTGeometry;
 
 namespace UnitTestGDTGeometry
 {
@@ -7,8 +8,13 @@ namespace UnitTestGDTGeometry
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        
+        public void TestCreatePoint()
         {
+            GDTGeometry.Feature.Point a = new GDTGeometry.Feature.Point(1, 2, 3);
+            Assert.AreEqual(1, a.X);
+            Assert.AreEqual(2, a.Y);
+            Assert.AreEqual(3, a.Z);
         }
     }
 }
