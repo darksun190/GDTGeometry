24f39ec107e0d5382a968c8b8813c3979618d034
sun xin <darksun190@hotmail.com>
2017-07-29 20:53:35 +0800

    删除ms unit test框架测试项目，添加Nunit框架测试项目

---
+++ GDTGeometryTests/Feature/PointTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using GDTGeometry.Feature;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDTGeometry.Feature.Tests
+{
+    [TestFixture()]
+    public class PointTests
+    {
+        [Test()]
+        
+        public void PointTest()
+        {
+            ///create test
+            Point A = new Point(1, 2, 3);
+            Assert.AreEqual(1, A.X);
+            Assert.AreEqual(2, A.Y);
+            Assert.AreEqual(3, A.Z);
+
+        }
+    }
+}
