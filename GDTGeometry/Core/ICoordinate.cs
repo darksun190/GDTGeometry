b755ebf6ebf051777e9a93fad96c1c66ea530b1c
sun xin <darksun190@hotmail.com>
2017-08-02 21:43:47 +0800

    创建坐标系Interface

---
+++ GDTGeometry/Core/ICoordinate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace GDTGeometry.Core
+{
+    /// <summary>
+    /// 坐标系本质是一个4X4矩阵，点（Vector）与矩阵相乘得到坐标变换后的新点（Vector）
+    /// </summary>
+    interface ICoordinate
+    {
+        
+    }
+}
