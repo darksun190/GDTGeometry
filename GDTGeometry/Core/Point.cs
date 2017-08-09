685c5f816ecd987121319804f4498d160741b4f5
sun xin <darksun190@hotmail.com>
2017-08-09 22:23:53 +0800

    手动合并Point类

--- GDTGeometry/Core/Point.cs
+++ GDTGeometry/Core/Point.cs
@@ -49,5 +49,15 @@ namespace GDTGeometry.Core
             Pos = new Position(P.X, P.Y, P.Z);
             Vec = new Vector(V.I, V.J, V.K);
         }
+        public Point Coordinate(Core.ICoordinate Coord)
+        {
+            DenseVector v = DenseVector.OfArray(new double[] { X, Y, Z });
+            var r = v * Coord.Matrix;
+            return new Point(r[0], r[1], r[2]);
+        }
+        public override string ToString()
+        {
+            return string.Format("x:{0}\ty:{1}\tz:{2}\n", X, Y, Z);
+        }
     }
 }
