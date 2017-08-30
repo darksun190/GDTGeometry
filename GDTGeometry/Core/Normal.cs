7e5ec167791a46678856c30504df6ed3198cba44
darksun <darksun190@gmail.com>
2017-08-30 08:52:48 +0800

    分离成向量和单位向量

---
+++ GDTGeometry/Core/Normal.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace GDTGeometry.Core
+{
+    /// <summary>
+    /// a vector which length = 1
+    /// </summary>
+    class Normal : Vector
+    {
+        double i, j, k;
+
+        public double I { get => i; set => i = value; }
+        public double J { get => j; set => j = value; }
+        public double K { get => k; set => k = value; }
+
+        public Normal(double a, double b, double c)
+            : base(a, b, c)
+        {
+            var len = Vec.Normalize(2);
+            I = len[0];
+            J = len[1];
+            K = len[2];
+        }
+        public DenseVector NormalVec
+        {
+            get
+            {
+                return DenseVector.OfArray(new double[] { I, J, K });
+            }
+        }
+        public DenseVector NormalVec4
+        {
+            get
+            {
+                return DenseVector.OfArray(new double[] { I, J, K, 0});
+            }
+        }
+        public static Vector operator *(double k, Normal a)
+        {
+            return new Vector(k * a.I, k * a.J, k * a.K);
+        }
+        public static Normal operator *(CartesianCoordinate M, Normal v)
+        {
+            var result = M.Matrix * v.Vec4;
+            return new Normal(result[0], result[1], result[2]);
+        }
+      
+    }
+}
--- GDTGeometry/Core/Vector.cs
+++ GDTGeometry/Core/Vector.cs
@@ -38,9 +38,17 @@ namespace GDTGeometry.Core
                 return DenseVector.OfArray(new double[] { A, B, C, 0 });
             }
         }
-        public static Position operator *(double k, Vector a)
+        public static Vector operator +(Vector v1, Vector v2)
         {
-            return new Position(k * a.a, k * a.b, k * a.c);
+            return new Vector(v1.A + v2.A, v1.B + v2.B, v1.C + v2.C);
+        }
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            return new Vector(v1.A - v2.A, v1.B - v2.B, v1.C - v2.C);
+        }
+        public static Vector operator *(double k, Vector a)
+        {
+            return new Vector(k * a.A, k * a.B, k * a.C);
         }
         public static Vector operator *(CartesianCoordinate M, Vector v)
         {
@@ -55,17 +63,23 @@ namespace GDTGeometry.Core
 
         public static double AngleBetweenTwoVector(Vector v1, Vector v2)
         {
-            double valueUp = v1.Vec * v2.Vec;
-            double valueDown = v1.Vec.L2Norm() * v2.Vec.L2Norm();
-            return Math.Atan2(valueUp, valueDown);
+            double valueUp = v1.DotProduct(v2);
+            double valueDown = v1.Length * v2.Length;
+            return Math.Acos(valueUp/valueDown);
         }
-
+        public double Length
+        {
+            get
+            {
+                return Vec.L2Norm();
+            }
+        }
         public override string ToString()
         {
             return string.Format("Vector [{0:F4}, {1:F4}, {2:F4}]\n", A, B, C);
         }
 
-        public Vector CrossMultiply(Vector b)
+        public Vector CrossProduct(Vector b)
         {
             var result = new Vector(
                 this.B * b.C - this.C * b.B,
@@ -74,6 +88,9 @@ namespace GDTGeometry.Core
                 );
             return result;
         }
-
+        public double DotProduct(Vector b)
+        {
+            return (this.A * b.A) + (this.B * b.B) + (this.C * b.C);
+        }
     }
 }
